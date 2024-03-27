7c67e7143c2e6eac56ac183595979d9a71805b37
Javier Escudero BUS <95212183+jescuderobus@users.noreply.github.com>
2024-03-27 12:45:37 +0100

    info0

--- js/main_index.js
+++ js/main_index.js
@@ -36,5 +36,33 @@ document.addEventListener('DOMContentLoaded', actualizarHora);
 
 
 
+/* Modal de información */ 
+
+// Obtiene el modal
+var modal = document.getElementById("infoModal");
+
+// Obtiene el botón que abre el modal
+var btn = document.querySelector(".mdi-information-slab-circle");
+
+// Obtiene el elemento <span> que cierra el modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario hace clic en el botón, abre el modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Cuando el usuario hace clic en <span> (x), cierra el modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Cuando el usuario hace clic en cualquier lugar fuera del modal, lo cierra
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 
